3e90b852888b95a285edd6f06a00bb151deb18ad
Tom Kirchner <tjkirch+github@gmail.com>
2011-04-07 01:41:28 -0400

    Actually get quest ID from event for Quests.accept
    Also make sure they're not on the quest before accepting it

--- events/quests.rb
+++ events/quests.rb
@@ -2,7 +2,7 @@
 # We only convert to a real Quest object as needed.
 module Quests
   def self.accept(event, player, room)
-    player.add_quest(quest)
+    player.add_quest(event[:quest_id]) if not player.on_quest?(event[:quest_id])
   end
 
   def self.abandon(event, player, room)
